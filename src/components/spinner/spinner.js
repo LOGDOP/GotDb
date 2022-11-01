79906fa3d730dae9671cdbb4dfbdef542c694c83
LOGDOP <nikitapodgol@gmail.com>
2022-11-02 01:18:04 +0300

    RandomCharacter

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-double-ring-ei9cqw70n9u"><div className="ldio-34384ip17c1">
+        <div></div>
+        <div></div>
+        <div><div></div></div>
+        <div><div></div></div>
+        </div></div>
+
+    )
+} 
+
+export default Spinner;
+
+
+
